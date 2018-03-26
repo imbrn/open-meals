e68861e4ae9560e8b72abf0a615ea2d8d1ab789a
Bruno <brunodev02221@gmail.com>
2018-03-26 10:37:33 -0300

    Add normalizeMeal function
    This function provide a method for converting the meal object returned
    by the `themealdb` to be normalized as expected for the application.

---
+++ src/api/normalizeMeal.spec.js
@@ -0,0 +1,92 @@
+import normalizeMeal from "./normalizeMeal";
+
+describe("normalizeMeal", () => {
+  let rawMeal;
+  let ingredients;
+  let measures;
+  let instructions;
+  let tags;
+  let normalizedMeal;
+
+  beforeAll(() => {
+    rawMeal = {
+      idMeal: "1234",
+      strMeal: "Name",
+      strCategory: "Category",
+      strArea: "Area",
+      strTags: "Tag1, Tag 2, Tag 3",
+      strYoutube: "https://youtube.com/watch?v=somevideoid",
+      strMealThumb: "https://somelocationforimages.com/imageid"
+    };
+    ingredients = mockIngredients(rawMeal, 15);
+    measures = mockMeasures(rawMeal, 15);
+    instructions = mockInstructions(rawMeal, 10);
+    tags = mockTags(rawMeal, 5);
+  });
+
+  it("normalizes keys", () => {
+    normalizedMeal = normalizeMeal(rawMeal);
+    expect(normalizedMeal.id).toEqual(rawMeal.idMeal);
+    expect(normalizedMeal.name).toEqual(rawMeal.strMeal);
+    expect(normalizedMeal.category).toEqual(rawMeal.strCategory);
+    expect(normalizedMeal.area).toEqual(rawMeal.strArea);
+    expect(normalizedMeal.ingredients).toEqual(
+      ingredients.map((ingredient, i) => ({
+        ingredient,
+        measure: measures[i]
+      }))
+    );
+    expect(normalizedMeal.instructions).toEqual(instructions);
+    expect(normalizedMeal.tags).toEqual(tags);
+    expect(normalizedMeal.youtube).toEqual(rawMeal.strYoutube);
+    expect(normalizedMeal.thumb).toEqual(rawMeal.strMealThumb);
+  });
+});
+
+function mockIngredients(receiver, quantity) {
+  const ingredients = [];
+  for (let i = 0; i < Math.max(20, quantity); i++) {
+    const j = i + 1;
+    let ingredient = null;
+    if (i < quantity) {
+      ingredient = `Ingredient ${j}`;
+      ingredients.push(ingredient);
+    }
+    receiver[`ingredient${j}`] = ingredient;
+  }
+  return ingredients;
+}
+
+function mockMeasures(receiver, quantity) {
+  const measures = [];
+  for (let i = 0; i < Math.max(20, quantity); i++) {
+    const j = i + 1;
+    let measure = null;
+    if (i < quantity) {
+      measure = `Measure ${j}`;
+      measures.push(measure);
+    }
+    receiver[`measure${j}`] = measure;
+  }
+  return measures;
+}
+
+function mockInstructions(receiver, quantity) {
+  const instructions = [];
+  for (let i = 0; i < quantity; i++) {
+    const step = `Step ${i + 1}`;
+    instructions.push(step);
+  }
+  receiver["strInstructions"] = instructions.join("\r\n");
+  return instructions;
+}
+
+function mockTags(receiver, quantity) {
+  const tags = [];
+  for (let i = 0; i < quantity; i++) {
+    const tag = `Tag ${i + 1}`;
+    tags.push(tag);
+  }
+  receiver["strTags"] = tags.join(", ");
+  return tags;
+}
