c4d61661886a217bb42a428e7ea0dc93b9509feb
Bruno <brunodev02221@gmail.com>
2018-04-19 14:22:38 -0300

    Add Github project link to navigation bar

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,23 @@
 module.exports = {
-    "parser": "babel-eslint",
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "node": true,
-        "jest": true,
-        "cypress/globals": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:prettier/recommended",
-    ],
-    "plugins": [
-        "react",
-        "cypress"
-    ],
-    "rules": {
-        "react/prop-types": "off"
-    }
+  parser: "babel-eslint",
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+    jest: true,
+    "cypress/globals": true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended"
+  ],
+  plugins: ["react", "cypress"],
+  rules: {
+    "react/prop-types": "off"
+  },
+  globals: {
+    GITHUB_PROJECT_LINK: true
+  }
 };
--- webpack/config.common.js
+++ webpack/config.common.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const CleanPlugin = require("clean-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
   output: {
@@ -8,13 +9,16 @@ module.exports = {
     publicPath: "/"
   },
   module: {
-    rules: [
-      { test: /\.js$/, exclude: /node_modules/, use: "babel-loader" }
-    ]
+    rules: [{ test: /\.js$/, exclude: /node_modules/, use: "babel-loader" }]
   },
   plugins: [
     new CleanPlugin(["../dist", "../build"], {
       allowExternal: true
+    }),
+    new webpack.DefinePlugin({
+      GITHUB_PROJECT_LINK: JSON.stringify(
+        "https://github.com/bruno02221/open-meals"
+      )
     })
   ]
-}
+};
