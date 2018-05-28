735f8ef3fb30c9c8f66e26b19bcb982825a72c48
Bruno <brunodev02221@gmail.com>
2018-05-27 21:27:59 -0300

    Remove `cypress` unused `eslint` plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,15 +5,14 @@ module.exports = {
     commonjs: true,
     es6: true,
     node: true,
-    jest: true,
-    "cypress/globals": true
+    jest: true
   },
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:prettier/recommended"
   ],
-  plugins: ["react", "cypress"],
+  plugins: ["react"],
   rules: {
     "react/prop-types": "off"
   },
